192403b1711e33894f55229c235682f84234ebd1
HieuNguyen1905 <120067573+HieuNguyen1905@users.noreply.github.com>
2023-03-16 14:06:25 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # password-generator
+
+
+## User Story
+
+- As a new developer, I want to build a application that can generate randomly a secure password for users and my self to protect our sensitive information.
+
+## Description
+
+- This application will prompt the user opinion whether or not what they want to include in their password.
+
+- This application will ask if the user would like to include how many character in the password (8-128), if they want to include any number, lowercase characters, uppercase characters or any special characters.
+
+- A pop-up window will appear on the website and user can click ok to accept and cancel button for no when the application ask for the options
+
+
+## Usage
+
+This application has HTML deloy. Please right click on the index.HTML and choose open in default browser
+Link to deloy application: https://hieunguyen1905.github.io/Module1-Horiseon-Code-Refactor/
+
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Globle Scope
 var specialCharacters = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-"~", "*", "?", ":","\"","\\"];
-var numbericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+    "~", "*", "?", ":", "\"", "\\"];
+var numbericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var emptyArray1 = [];
 var emptyArray2 = [];
 var emptyArray3 = [];
@@ -11,45 +11,45 @@ var emptyArray4 = [];
 //var availableCharacters = emptyArray1.concat(emptyArray2,emptyArray3,emptyArray4);
 var generateBtn = document.querySelector("#generate");
 
-function writePassword(){
+function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.textContent = password;
 }
 
-generateBtn.addEventListener("click",writePassword);
+generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-    var length = checkLength ();
+function generatePassword() {
+    var length = checkLength();
     var number = checkNum();
     var lowerCase = checkLower();
     var upperCase = checkUpper();
     var special = checkSpecial();
-    if(number){
+    if (number) {
         var availableArray1 = numbericCharacters;
     }
-    if(lowerCase){
+    if (lowerCase) {
         var availableArray2 = lowerCasedCharacters;
     }
-    if(upperCase){
+    if (upperCase) {
         var availableArray3 = upperCasedCharacters;
     }
-    if(special){
+    if (special) {
         var availableArray4 = specialCharacters;
     }
-    if(!number && !lowerCase && !upperCase && !special){
+    if (!number && !lowerCase && !upperCase && !special) {
         alert("Plese choose at least 1 type of character. ")
         generatePassword();
     }
     var availableCharacters = [];
-    availableCharacters = availableCharacters.concat(availableArray1,availableArray2,availableArray3,availableArray4);
+    availableCharacters = availableCharacters.concat(availableArray1, availableArray2, availableArray3, availableArray4);
     //console.log(availableCharacters);
 
-    var filtered = availableCharacters.filter(function(element){
+    var filtered = availableCharacters.filter(function (element) {
         return element !== undefined;
     })
     var result = "";
-    for (i=1; i <= length; i++){
+    for (i = 1; i <= length; i++) {
         var index = Math.floor(Math.random() * filtered.length);
         var computerChoice = filtered[index];
         result = result.concat(computerChoice);
@@ -57,41 +57,45 @@ function generatePassword(){
     return result;
 
 }
-function checkLength(){
+function checkLength() {
     var userChoiceLength = parseInt(prompt("Enter the length of your password (Must be at least 8 characters and not excess 128 charactors): "));
-    if (userChoiceLength<8){
+    console.log(userChoiceLength)
+    if (userChoiceLength < 8) {
         alert("Your password must include at least 8 characters ");
         checkLength();
-    }else if (userChoiceLength>128){
+    } else if (userChoiceLength > 128) {
         alert("Your password must include maximum 128 characters ");
         checkLength();
+    } else {
+        console.log(userChoiceLength);
+        return userChoiceLength;
+        
     }
-    return userChoiceLength;
 }
 
-function checkNum(){
+function checkNum() {
     var userChoiceNum = confirm("Password includes number? ");
-    if (userChoiceNum){
+    if (userChoiceNum) {
         return true;
     }
 }
 
-function checkLower(){
+function checkLower() {
     var userChoiceLower = confirm("Password includes lowercased characters ? ");
-    if (userChoiceLower){
+    if (userChoiceLower) {
         return true;
     }
 }
 
-function checkUpper(){
+function checkUpper() {
     var userChoiceUpper = confirm("Password includes uppercased characters ? ");
-    if (userChoiceUpper){
+    if (userChoiceUpper) {
         return true;
     }
 }
-function checkSpecial(){
+function checkSpecial() {
     var userChoiceSpecial = confirm("Password includes special characters ? ");
-    if (userChoiceSpecial){
+    if (userChoiceSpecial) {
         return true;
     }
 }
